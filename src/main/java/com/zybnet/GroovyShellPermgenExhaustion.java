1dda9569b6c5d22993168536567cff1d1fb62d9f
Raffaele Sgarro <raffaelesgarro@gmail.com>
2013-07-06 00:31:44 +0200

    Main Java source file

---
+++ src/main/java/com/zybnet/GroovyShellPermgenExhaustion.java
@@ -0,0 +1,16 @@
+package com.zybnet;
+
+import groovy.lang.GroovyShell;
+
+public class GroovyShellPermgenExhaustion {
+  
+  public static void main(String ...args) {
+    
+	GroovyShell shell = new GroovyShell();
+    
+	while ( true ) {
+	  shell.evaluate( " answer = 42 " );
+	}
+  }
+  
+}
